c6347aaa8633f79e7e821e7ed3c9549469c29d91
Jim Wright <jimwright808@gmail.com>
2018-12-14 12:55:20 +0400

    Hardcode values for ListingInfo and set basic layout

--- src/ViewListingInfo.js
+++ src/ViewListingInfo.js
@@ -1,5 +1,81 @@
 import React, { Fragment } from "react";
+import { List, Grid, Icon } from "semantic-ui-react";
 
-const ViewListingInfo = () => {};
+const ViewListingInfo = ({
+  vaccinated,
+  goodWithKids,
+  goodWithDogs,
+  goodWithCats,
+  houseTrained,
+  leashTrained,
+  crateTrained,
+  litterTrained,
+}) => (
+  <Fragment>
+    <h3>My Info</h3>
+    <Grid>
+      <Grid.Row>
+        <Grid.Column width={5}>
+          <List.Item>
+            <List.Icon name="check" />
+            <List.Content>Spayed/Neutered</List.Content>
+          </List.Item>
+        </Grid.Column>
+        <Grid.Column width={5}>
+          <List.Item>
+            <List.Icon name="check" />
+            <List.Content>Good with kids</List.Content>
+          </List.Item>
+        </Grid.Column>
+        <Grid.Column width={5}>
+          <List.Item>
+            <List.Icon name="times" />
+            <List.Content>Leash trained</List.Content>
+          </List.Item>
+        </Grid.Column>
+      </Grid.Row>
+      <Grid.Row>
+        <Grid.Column width={5}>
+          <List.Item>
+            <List.Icon name="check" />
+            <List.Content>Vaccinated</List.Content>
+          </List.Item>
+        </Grid.Column>
+        <Grid.Column width={5}>
+          <List.Item>
+            <List.Icon name="check" />
+            <List.Content>Good with dogs</List.Content>
+          </List.Item>
+        </Grid.Column>
+        <Grid.Column width={5}>
+          <List.Item>
+            <List.Icon name="check" />
+            <List.Content>Crate trained</List.Content>
+          </List.Item>
+        </Grid.Column>
+      </Grid.Row>
+      <Grid.Row>
+        <Grid.Column width={5}>
+          <List.Item>
+            <List.Icon name="check" />
+            <List.Content>House trained</List.Content>
+          </List.Item>
+        </Grid.Column>
+        <Grid.Column width={5}>
+          <List.Item>
+            <List.Icon name="times" />
+            <List.Content>Good with cats</List.Content>
+          </List.Item>
+        </Grid.Column>
+        <Grid.Column width={5}>
+          <List.Item>
+            <List.Icon name="check" />
+            <List.Content>Litter trained</List.Content>
+          </List.Item>
+        </Grid.Column>
+      </Grid.Row>
+    </Grid>
+  </Fragment>
+);
 
 export default ViewListingInfo;
