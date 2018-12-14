f3c4bc943cc383611f592d98e73b1754ee9680e9
Jim Wright <jimwright808@gmail.com>
2018-12-14 13:07:32 +0400

    Hardcode content for ListingStory

--- src/ViewListingStory.js
+++ src/ViewListingStory.js
@@ -1,5 +1,36 @@
 import React, { Fragment } from "react";
+import { Container } from "semantic-ui-react";
 
-const ViewListingStory = () => {};
+const ViewListingStory = ({ story }) => (
+  <Fragment>
+    <Container>
+      <h3>My story</h3>
+      <p>
+        Lorem ipsum dolor amet eiusmod ea bicycle rights asymmetrical intelligentsia craft
+        beer. Semiotics messenger bag quinoa, poutine tumeric plaid fingerstache consequat
+        migas. Pickled wayfarers green juice disrupt intelligentsia man bun microdosing.
+        Freegan adipisicing godard tumblr, activated charcoal sustainable dolore forage
+        echo park kombucha af gochujang. Heirloom everyday carry banh mi ad est. Butcher
+        asymmetrical +1 vape ut, glossier everyday carry anim bicycle rights 8-bit.
+        Commodo flexitarian food truck YOLO forage subway tile cronut gluten-free fam
+        shaman cornhole VHS paleo.
+      </p>
+      <p>
+        Pabst irure PBR&B quis kogi migas gochujang chartreuse neutra tote bag. Plaid
+        hella enamel pin skateboard anim. Deserunt velit offal marfa non. Readymade sunt
+        laborum, forage chia brunch ad knausgaard squid ugh beard. Vice meh ut, kogi
+        fugiat poutine swag cillum XOXO microdosing labore. Poutine echo park
+        reprehenderit elit pug.
+      </p>
+      <p>
+        Health goth 8-bit knausgaard portland, nostrud shaman commodo. Nostrud duis
+        mustache before they sold out polaroid, migas in ad meh tumeric aute. Raclette
+        cillum activated charcoal, snackwave proident semiotics do et banh mi dolore dolor
+        williamsburg cliche ut aliquip. Slow-carb dolore non gastropub plaid shaman
+        waistcoat jianbing church-key mollit poutine trust fund.
+      </p>
+    </Container>
+  </Fragment>
+);
 
 export default ViewListingStory;
