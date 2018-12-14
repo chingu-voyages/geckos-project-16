d4046518f9fa50f9b29288fe8ea21456bb436bff
Jim Wright <jimwright808@gmail.com>
2018-12-14 11:33:20 +0400

    Hardcode values to ViewListingFacts to get basic layout

--- src/ViewListingFacts.js
+++ src/ViewListingFacts.js
@@ -1,12 +1,58 @@
 import React, { Fragment } from "react";
+import { List, Grid } from "semantic-ui-react";
 
 const ViewListingFacts = ({ breed, gender, age, color, weight, location }) => (
   <Fragment>
-    <h3>My name is Test Dog!</h3>
-    <Image src={imageMain} alt="Main image of pet" />
-    <Image src={image2} alt="Additional image of pet" />
-    <Image src={image3} alt="Additional image of pet" />
-    <Image src={image4} alt="Additional image of pet" />
+    <Grid>
+      <Grid.Row>
+        <Grid.Column width={3}>
+          <List.Item>
+            <List.Content>
+              <strong>Breed:</strong> Husky
+            </List.Content>
+          </List.Item>
+        </Grid.Column>
+        <Grid.Column width={3}>
+          <List.Item>
+            <List.Content>
+              <strong>Color:</strong> Black
+            </List.Content>
+          </List.Item>
+        </Grid.Column>
+      </Grid.Row>
+      <Grid.Row>
+        <Grid.Column width={3}>
+          <List.Item>
+            <List.Content>
+              <strong>Gender:</strong> Female
+            </List.Content>
+          </List.Item>
+        </Grid.Column>
+        <Grid.Column width={3}>
+          <List.Item>
+            <List.Content>
+              <strong>Weight:</strong> 8kg
+            </List.Content>
+          </List.Item>
+        </Grid.Column>
+      </Grid.Row>
+      <Grid.Row>
+        <Grid.Column width={3}>
+          <List.Item>
+            <List.Content>
+              <strong>Age:</strong> 3 years
+            </List.Content>
+          </List.Item>
+        </Grid.Column>
+        <Grid.Column width={3}>
+          <List.Item>
+            <List.Content>
+              <strong>Location:</strong> Busan
+            </List.Content>
+          </List.Item>
+        </Grid.Column>
+      </Grid.Row>
+    </Grid>
   </Fragment>
 );
 
