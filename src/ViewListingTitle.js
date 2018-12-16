21a15e7c74d2df674fa455714b9f71b5e34c9b94
Jim Wright <jimwright808@gmail.com>
2018-12-16 15:41:59 +0400

    Consolidate ViewListingTitle and ViewListingFacts. Get basic layout

--- src/ViewListingTitle.js
+++ src/ViewListingTitle.js
@@ -1,14 +1,66 @@
 import React, { Fragment } from "react";
-import { Image, Container } from "semantic-ui-react";
+import { Image, Container, Grid, List, Divider } from "semantic-ui-react";
 
-const ViewListingTitle = ({ petName, imageMain, image2, image3, image4 }) => (
+const ViewListingTitle = ({
+  petName,
+  imageMain,
+  image2,
+  image3,
+  image4,
+  breed,
+  gender,
+  age,
+  color,
+  weight,
+  location,
+}) => (
   <Fragment>
     <Container>
-      <h3>{`My name is ${petName}!`}</h3>
-      <Image src={imageMain} alt="Main image of pet" />
-      <Image src={image2} alt="Additional image of pet" className="subImage" />
-      <Image src={image3} alt="Additional image of pet" className="subImage" />
-      <Image src={image4} alt="Additional image of pet" className="subImage" />
+      <h2>{`My name is ${petName}!`}</h2>
+      <Grid columns={4}>
+        <Grid.Row stretched>
+          <Grid.Column width={8}>
+            <Image src={imageMain} alt="Main image of pet" className="imageMain" />
+          </Grid.Column>
+          <Grid.Column>
+            <List.Content>
+              <strong>Breed:</strong> {breed}
+            </List.Content>
+            <List.Content>
+              <strong>Gender:</strong> {gender}
+            </List.Content>
+            <List.Content>
+              <strong>Age:</strong> {age}
+            </List.Content>
+          </Grid.Column>
+          <Grid.Column>
+            <List.Content>
+              <strong>Color:</strong> {color}
+            </List.Content>
+            <List.Content>
+              <strong>Weight:</strong> {weight}
+            </List.Content>
+            <List.Content>
+              <strong>Location:</strong> {location}
+            </List.Content>
+          </Grid.Column>
+        </Grid.Row>
+        <Grid.Row>
+          <Grid.Column width={2}>
+            <Image src={imageMain} alt="Additional image of pet" className="subImage" />
+          </Grid.Column>
+          <Grid.Column width={2}>
+            <Image src={image2} alt="Additional image of pet" className="subImage" />
+          </Grid.Column>
+          <Grid.Column width={2}>
+            <Image src={image3} alt="Additional image of pet" className="subImage" />
+          </Grid.Column>
+          <Grid.Column width={2}>
+            <Image src={image4} alt="Additional image of pet" className="subImage" />
+          </Grid.Column>
+        </Grid.Row>
+      </Grid>
+      <Divider />
     </Container>
   </Fragment>
 );
