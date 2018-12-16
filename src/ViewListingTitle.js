02be971d5267183c3a7ad1bbb8a1fa6e793dbb0b
Jim Wright <jimwright808@gmail.com>
2018-12-16 10:42:54 +0400

    Add className to sub images

--- src/ViewListingTitle.js
+++ src/ViewListingTitle.js
@@ -6,9 +6,9 @@ const ViewListingTitle = ({ petName, imageMain, image2, image3, image4 }) => (
     <Container>
       <h3>{`My name is ${petName}!`}</h3>
       <Image src={imageMain} alt="Main image of pet" />
-      <Image src={image2} alt="Additional image of pet" />
-      <Image src={image3} alt="Additional image of pet" />
-      <Image src={image4} alt="Additional image of pet" />
+      <Image src={image2} alt="Additional image of pet" className="subImage" />
+      <Image src={image3} alt="Additional image of pet" className="subImage" />
+      <Image src={image4} alt="Additional image of pet" className="subImage" />
     </Container>
   </Fragment>
 );
