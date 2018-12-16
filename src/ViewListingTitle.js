4412b41f0cab483f35ee586575576c678f89d8c3
Jim Wright <jimwright808@gmail.com>
2018-12-16 10:13:37 +0400

    Remove hardcoded name value and link to test data

--- src/ViewListingTitle.js
+++ src/ViewListingTitle.js
@@ -1,10 +1,10 @@
 import React, { Fragment } from "react";
 import { Image, Container } from "semantic-ui-react";
 
-const ViewListingTitle = ({ name, imageMain, image2, image3, image4 }) => (
+const ViewListingTitle = ({ petName, imageMain, image2, image3, image4 }) => (
   <Fragment>
     <Container>
-      <h3>My name is Test Dog!</h3>
+      <h3>{`My name is ${petName}!`}</h3>
       <Image src={imageMain} alt="Main image of pet" />
       <Image src={image2} alt="Additional image of pet" />
       <Image src={image3} alt="Additional image of pet" />
