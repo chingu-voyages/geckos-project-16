5c8f8cd63dde75d13ecf0dac8a20a4dfa12ea05c
Jim Wright <jimwright808@gmail.com>
2018-12-17 14:39:28 +0400

    Amend spacing of call to action elements

--- src/ViewListingAction.js
+++ src/ViewListingAction.js
@@ -20,21 +20,23 @@ const ViewListingAction = ({ contactName, contactEmail, fee }) => (
       </List>
       <Grid>
         <Grid.Row className="contactDetails">
-          <Grid.Column width={8}>
+          <Grid.Column width={5}>
             <List.Item>
               <List.Content>
                 <strong>Contact Name:</strong> {contactName}
               </List.Content>
             </List.Item>
           </Grid.Column>
-          <List.Item>
-            <List.Content>
-              <strong>Contact Email:</strong> {contactEmail}
-            </List.Content>
-          </List.Item>
+          <Grid.Column width={5}>
+            <List.Item>
+              <List.Content>
+                <strong>Contact Email:</strong> {contactEmail}
+              </List.Content>
+            </List.Item>
+          </Grid.Column>
         </Grid.Row>
         <Grid.Row>
-          <Grid.Column width={8}>
+          <Grid.Column width={5}>
             <List.Item>
               <List.Content className="adoptionFee">
                 <strong>Adoption Fee:</strong> ₩{fee}
