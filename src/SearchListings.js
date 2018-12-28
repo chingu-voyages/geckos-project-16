3d9b2e5ad01ca1aa3930646d5d75578d5e737b33
Jim Wright <jimwright808@gmail.com>
2018-12-28 09:44:47 +0000

    Start to get basic structure  sorted

--- src/SearchListings.js
+++ src/SearchListings.js
@@ -2,24 +2,53 @@ import React, { Component, Fragment } from "react";
 import { Button, Container, Divider, Icon, Segment, Form } from "semantic-ui-react";
 
 class SearchListings extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      searchLocation: "",
-      searchGender: "",
-      searchAge: "",
-      searchColor: "",
-      searchShelter: "",
-      searchItems: 9,
-    };
-  }
+  state = {
+    searchLocation: "",
+    searchGender: "",
+    searchAge: "",
+    searchColor: "",
+    searchShelter: "",
+    searchItems: 9,
+  };
+
+  handleChange = (event, { value }) => {
+    const target = event.target;
+    this.setState({ value });
+  };
+
+  handleSubmit = event => {
+    event.preventDefault();
+  };
 
   render() {
+    const { value } = this.state;
     return (
       <Fragment>
         <Container id="container">
-          <Segment id="container-segment" textAlign="center">
-            <h2>Search Settings</h2>
+          <Segment id="container-segment">
+            <Form onSubmit={this.handleSubmit}>
+              <Form.Group grouped>
+                <label>Current Location</label>
+                <Form.Radio
+                  label="Small"
+                  value="sm"
+                  checked={value === "sm"}
+                  onChange={this.handleChange}
+                />
+                <Form.Radio
+                  label="Medium"
+                  value="md"
+                  checked={value === "md"}
+                  onChange={this.handleChange}
+                />
+                <Form.Radio
+                  label="Large"
+                  value="lg"
+                  checked={value === "lg"}
+                  onChange={this.handleChange}
+                />
+              </Form.Group>
+            </Form>
           </Segment>
         </Container>
       </Fragment>
