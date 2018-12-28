104502576088c5b7feb0886eb0e92c3589189912
harjas27 <harjasbathla@gmail.com>
2018-12-28 12:43:52 +0530

    MainForm structure added

--- src/MainForm.js
+++ src/MainForm.js
@@ -7,4 +7,50 @@ class MainForm extends Component {
   state = {
     step: 1,
   };
+
+  nextStep = () => {
+    const { step } = this.state;
+    this.setState({
+      step: step + 1,
+    });
+  };
+
+  prevStep = () => {
+    const { step } = this.state;
+    this.setState({
+      step: step - 1,
+    });
+  };
+
+  handleChange = input => event => {
+    this.setState({ [input]: event.target.value });
+  };
+
+  render() {
+    const { step } = this.state;
+    const { fields } = this.state;
+    const values = { fields };
+    switch (step) {
+      case 1:
+        return (
+          <ListingDetails
+            nextStep={this.nextStep}
+            handleChange={this.handleChange}
+            values={values}
+          />
+        );
+      case 2:
+        return (
+          <Confirmation
+            nextStep={this.nextStep}
+            prevStep={this.prevStep}
+            values={values}
+          />
+        );
+      case 3:
+        return <Success />;
+      default:
+      //do nothing
+    }
+  }
 }
