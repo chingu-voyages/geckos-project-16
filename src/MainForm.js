b3dde95b46c1c86bee81b186bf6379da00cdb1c1
harjas27 <harjasbathla@gmail.com>
2018-12-28 12:19:15 +0530

    Form initialised

---
+++ src/MainForm.js
@@ -0,0 +1,10 @@
+import React, { Component } from "react";
+import ListingDetails from "./ListingDetails";
+import Confirmation from "./Confirmation";
+import Success from "./Success";
+
+class MainForm extends Component {
+  state = {
+    step: 1,
+  };
+}
