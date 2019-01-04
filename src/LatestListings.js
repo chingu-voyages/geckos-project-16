6fcc5d48d5e7265f2d0114c5b2420fd3803157e4
Daniel Strong <danielstrong.ds@gmail.com>
2019-01-04 11:19:15 +0900

    WIP - filtration works, styling needed

--- src/LatestListings.js
+++ src/LatestListings.js
@@ -1,59 +1,114 @@
+// If there's more than 5, use a 'show more' accordion
+// If you using mobile, close the search box, but add an option to drop it down
+// When active it takes the user to the listings page
+
 import React, { Component } from "react";
 import CardList from "./CardList";
 import SearchBox from "./SearchBox";
+import { Grid } from "semantic-ui-react";
+
+// used to map through object
+const labels = ["location", "gender", "breed", "color"];
+
+// stringifying to eliminate obj references
+// use JSON.parse(fields)
+const fields = JSON.stringify({
+  location: {},
+  gender: {},
+  breed: {},
+  color: {},
+});
+
+// holds our checked box values
+const checkedObj = {
+  location: "",
+  gender: "",
+  breed: "",
+  color: "",
+};
 
 class LatestListings extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      displayPets: [],
-      filteredPets: [],
-      radiosChecked: [],
-    };
-  }
+  state = {
+    allPets: [],
+    filteredPets: [],
+    checkedObj,
+    searchFields: JSON.parse(fields),
+  };
 
   async componentDidMount() {
     try {
       const response = await fetch("http://localhost:4000/posts");
-      const data = await response.json();
-      this.setState({ displayPets: data, filteredPets: data });
+      const allPets = await response.json();
+      this.setState({ allPets }, this.handleLogic);
     } catch (err) {
       console.log(err);
     }
   }
 
-  searchClear = () => {
-    this.setState({
-      radiosChecked: [],
-    });
+  handleLogic = (
+    filteredPets = this.state.allPets,
+    checkedObj = this.state.checkedObj
+  ) => {
+    const searchFields = this.createSearchBoxes(filteredPets);
+    this.setState({ searchFields, filteredPets, checkedObj });
   };
 
-  onSearchChange = event => {
-    const value = event.target.value;
-    const name = event.target.name;
-    this.setState(
-      {
-        radiosChecked: [...this.state.radiosChecked, value],
-      },
-      this.handleFilter
-    );
-  };
-
-  handleFilter = () => {
-    const { filteredPets, radiosChecked } = this.state;
-    const filtered = filteredPets.filter(pet => {
-      return radiosChecked.some(name => {
-        return name === pet[name];
+  createSearchBoxes = petsArr =>
+    petsArr.reduce((acc, cVal) => {
+      labels.forEach(value => {
+        acc[value][cVal[value]]
+          ? (acc[value][cVal[value]] += 1)
+          : (acc[value][cVal[value]] = 1);
       });
-    });
+      return acc;
+    }, JSON.parse(fields));
+
+  handleBoxClick = (e, { value, name, checked }) => {
+    const updater = checked ? value : "";
+    const checkedObj = { ...this.state.checkedObj, [name]: updater };
+    this.handleFilter(name, value, checked, checkedObj);
   };
+
+  handleFilter = (name, value, checked, checkedObj) => {
+    const { filteredPets, allPets } = this.state;
+    const filteredResults = checked
+      ? // user checks a box - filter the filtered results further
+        filteredPets.filter(pet => pet[name] === value)
+      : // user unchecks a box - check if any boxes are checked at all
+      !labels.every(label => !checkedObj[label])
+      ? // if there is, filter all the pets again
+        allPets.filter(pet =>
+          labels.every(label =>
+            checkedObj[label] ? checkedObj[label] === pet[label] : true
+          )
+        )
+      : // if there isn't, return all pets
+        allPets;
+    this.handleLogic(filteredResults, checkedObj);
+  };
+
+  handleClear = () => this.handleLogic(this.state.allPets, checkedObj);
 
   render() {
+    const { filteredPets, searchFields, checkedObj } = this.state;
+    const searchParams = labels.map(label => ({
+      label,
+      values: Object.entries(searchFields[label]),
+    }));
     return (
-      <div>
-        <SearchBox searchChange={this.onSearchChange} />
-        <CardList pets={this.state.filteredPets} />
-      </div>
+      <Grid columns={16}>
+        <Grid.Column width={4}>
+          <SearchBox
+            searchParams={searchParams}
+            checkedObj={checkedObj}
+            searchChange={this.handleBoxClick}
+            searchClear={this.handleClear}
+          />
+        </Grid.Column>
+        <Grid.Column width={4}>
+          <CardList pets={filteredPets} />
+        </Grid.Column>
+      </Grid>
     );
   }
 }
--- src/SearchBox.js
+++ src/SearchBox.js
@@ -1,135 +1,36 @@
 import React from "react";
-import { Button, Container, Input, Segment, Form } from "semantic-ui-react";
+import { Header, Container, Segment, Form } from "semantic-ui-react";
 import "./SearchBox.css";
 
-const SearchBox = ({ searchChange, searchClear }) => {
-  return (
-    <Container id="search-container">
-      <Segment id="search-container-segment">
-        <Form>
-          <Form.Group grouped>
-            <label>
-              <h4>Location</h4>
-            </label>
-            <Form.Field inline>
-              <Input type="radio" name="location" value="seoul" onChange={searchChange} />
-              <label>Seoul</label>
-            </Form.Field>
-            <Form.Field inline>
-              <Input type="radio" name="location" value="busan" onChange={searchChange} />
-              <label>Busan</label>
-            </Form.Field>
-            <Form.Field inline>
-              <Input
-                type="radio"
-                name="location"
-                value="gwangju"
+const SearchBox = ({ searchParams, checkedObj, searchChange, searchClear }) => (
+  <Container id="search-container">
+    <Segment inverted color="pink" id="search-container-segment">
+      <Form loading={!searchParams[0].values.length}>
+        {searchParams.map(({ label, values }) => (
+          <Form.Group grouped key={label}>
+            <Header as="h4" color="violet" content={label} />
+            {values.map(([value, count]) => (
+              <Form.Checkbox
+                key={value}
+                color="violet"
+                name={label}
+                value={value}
+                checked={checkedObj[label] === value}
+                label={`${value} (${count})`}
                 onChange={searchChange}
               />
-              <label>Gwangju</label>
-            </Form.Field>
-            <Form.Field inline>
-              <Input type="radio" name="location" value="jeju" onChange={searchChange} />
-              <label>Jeju</label>
-            </Form.Field>
-            <Form.Field inline>
-              <Input type="radio" name="location" value="suwon" onChange={searchChange} />
-              <label>Suwon</label>
-            </Form.Field>
+            ))}
           </Form.Group>
-          <Form.Group grouped>
-            <label>
-              <h4>Gender</h4>
-            </label>
-            <Form.Field inline>
-              <Input type="radio" name="gender" value="male" onChange={searchChange} />
-              <label>Male</label>
-            </Form.Field>
-            <Form.Field inline>
-              <Input type="radio" name="gender" value="female" onChange={searchChange} />
-              <label>Female</label>
-            </Form.Field>
-          </Form.Group>
-          <Form.Group grouped>
-            <label>
-              <h4>Breed</h4>
-            </label>
-            <Form.Field inline>
-              <Input type="radio" name="breed" value="husky" onChange={searchChange} />
-              <label>Husky</label>
-            </Form.Field>
-            <Form.Field inline>
-              <Input type="radio" name="breed" value="bulldog" onChange={searchChange} />
-              <label>Bulldog</label>
-            </Form.Field>
-            <Form.Field inline>
-              <Input type="radio" name="breed" value="labrador" onChange={searchChange} />
-              <label>Labrador</label>
-            </Form.Field>
-            <Form.Field inline>
-              <Input type="radio" name="breed" value="boxer" onChange={searchChange} />
-              <label>Boxer</label>
-            </Form.Field>
-            <Form.Field inline>
-              <Input type="radio" name="breed" value="spaniel" onChange={searchChange} />
-              <label>Spaniel</label>
-            </Form.Field>
-          </Form.Group>
-          <Form.Group grouped>
-            <label>
-              <h4>Age</h4>
-            </label>
-            <Form.Field inline>
-              <Input type="radio" name="age" value="0" onChange={searchChange} />
-              <label>Under 1 year</label>
-            </Form.Field>
-            <Form.Field inline>
-              <Input type="radio" name="age" value="1-3" onChange={searchChange} />
-              <label>1-3 years</label>
-            </Form.Field>
-            <Form.Field inline>
-              <Input type="radio" name="age" value="4-6" onChange={searchChange} />
-              <label>4-6 years</label>
-            </Form.Field>
-            <Form.Field inline>
-              <Input type="radio" name="age" value="7-9" onChange={searchChange} />
-              <label>7-9 years</label>
-            </Form.Field>
-            <Form.Field inline>
-              <Input type="radio" name="age" value="10" onChange={searchChange} />
-              <label>Over 10 years</label>
-            </Form.Field>
-          </Form.Group>
-          <Form.Group grouped>
-            <label>
-              <h4>Color</h4>
-            </label>
-            <Form.Field inline>
-              <Input type="radio" name="color" value="black" onChange={searchChange} />
-              <label>Black</label>
-            </Form.Field>
-            <Form.Field inline>
-              <Input type="radio" name="color" value="white" onChange={searchChange} />
-              <label>White</label>
-            </Form.Field>
-            <Form.Field inline>
-              <Input type="radio" name="color" value="brown" onChange={searchChange} />
-              <label>Brown</label>
-            </Form.Field>
-            <Form.Field inline>
-              <Input type="radio" name="color" value="yellow" onChange={searchChange} />
-              <label>Yellow</label>
-            </Form.Field>
-            <Form.Field inline>
-              <Input type="radio" name="color" value="grey" onChange={searchChange} />
-              <label>Grey</label>
-            </Form.Field>
-          </Form.Group>
-          <Button onClick={searchClear}>Clear filters</Button>
-        </Form>
-      </Segment>
-    </Container>
-  );
-};
+        ))}
+        <Form.Button
+          size="big"
+          color="purple"
+          content="Unselect All"
+          onClick={searchClear}
+        />
+      </Form>
+    </Segment>
+  </Container>
+);
 
 export default SearchBox;
