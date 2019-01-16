4349e5ff78c53981bf4975a0dd3458f84b5a0681
Dhairya <dhairiya96@gmail.com>
2019-01-16 14:38:35 +0530

    replaced h3 with header

--- src/ErrorNotFound.js
+++ src/ErrorNotFound.js
@@ -1,5 +1,5 @@
 import React, { Component } from "react";
-import { Container, Segment, Image } from "semantic-ui-react";
+import { Container, Segment, Image, Header } from "semantic-ui-react";
 import "./ErrorNotFound.css";
 export default class ErrorNotFound extends Component {
   render() {
@@ -7,9 +7,12 @@ export default class ErrorNotFound extends Component {
       <Container className="er-container">
         <Segment inverted color="pink" className="er-segment">
           <Segment>
-            <h3 className="er-heading">
-              Sorry, we couldn't find what you're looking for.
-            </h3>
+            <Header
+              className="er-heading"
+              as="h3"
+              color="purple"
+              content="Sorry, we couldn't find what you're looking for."
+            />
             <Image src="/error.svg" fluid />
           </Segment>
         </Segment>
