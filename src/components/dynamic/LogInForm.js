84d48538e6a302aee8cd811205280a39da50f663
Daniel Strong <danielstrong.ds@gmail.com>
2019-01-25 19:41:39 +0900

    working Facebook login

--- src/components/dynamic/LogInForm.js
+++ src/components/dynamic/LogInForm.js
@@ -1,5 +1,5 @@
-import React, { Component } from "react";
-import { Form } from "semantic-ui-react";
+import React, { Component, Fragment } from "react";
+import { Form, Divider, Button } from "semantic-ui-react";
 import { fetcher } from "../../helpers";
 import FormMessages from "../reusable/FormMessages";
 
@@ -47,6 +47,59 @@ class LogInForm extends Component {
     }
   };
 
+  handleFacebook = () => {
+    this.setState(
+      { isProcessing: true },
+      // use window to access the SDK we loaded in our HTML file
+      // use that SDKs login function
+      window.FB.login(async resp => {
+        if (resp.status === "connected") {
+          // if it's succcessful grab that users token
+          const token = resp.authResponse.accessToken;
+          const url = `https://graph.facebook.com/me?fields=email%20&access_token=${token}`;
+          try {
+            // use facebook's graph api to get your email
+            const blob = await fetch(url);
+            const resp = await blob.json();
+            if (!blob.ok) throw resp;
+            // if it's okay send that email to our API
+            const url2 = "http://localhost:3000/api/users/social";
+            const blob2 = await fetch(url2, {
+              headers: { "Content-Type": "application/json" },
+              method: "POST",
+              body: JSON.stringify({ email: resp.email }),
+            });
+            const user = await blob2.json();
+            if (!blob2.ok) throw user;
+            // checks if there's a user in the DB with that email
+            // if there is it'll send back an obj with everything we need
+            // put it in storage
+            localStorage.setItem("user", JSON.stringify(user));
+            // reset our state and show a successful message
+            this.setState(
+              {
+                ...initialState,
+                successStatus: true,
+                activeUser: user.fullName,
+              },
+              // send the user obj back to Navbar
+              // this will update the UI
+              // logout button, etc
+              () => this.props.handleUser(user)
+            );
+          } catch (err) {
+            console.log(err);
+            this.setState({
+              ...initialState,
+              errorStatus: true,
+              errorMsg: err.message,
+            });
+          }
+        }
+      })
+    );
+  };
+
   handleSubmit = e => {
     e.preventDefault();
     this.setState({ isProcessing: true }, this.handleLogIn);
@@ -63,43 +116,62 @@ class LogInForm extends Component {
       isProcessing,
     } = this.state;
     return (
-      <Form
-        loading={isProcessing}
-        className="auth-form"
-        success={successStatus}
-        error={errorStatus}
-        onSubmit={this.handleSubmit}
-      >
-        <Form.Group widths="equal">
-          <Form.Input
-            type="email"
-            placeholder="Email"
-            name="email"
-            value={email}
-            onChange={this.handleChange}
+      <Fragment>
+        <Form
+          loading={isProcessing}
+          className="auth-form"
+          success={successStatus}
+          error={errorStatus}
+          onSubmit={this.handleSubmit}
+        >
+          <Form.Group widths="equal">
+            <Form.Input
+              type="email"
+              placeholder="Email"
+              name="email"
+              value={email}
+              onChange={this.handleChange}
+            />
+            <Form.Input
+              type="password"
+              placeholder="Password"
+              name="password"
+              value={password}
+              onChange={this.handleChange}
+            />
+          </Form.Group>
+          <Form.Button
+            color="purple"
+            size="big"
+            type="submit"
+            content={isProcessing ? "Processing" : "Submit"}
+            disabled={!email || !password}
           />
-          <Form.Input
-            type="password"
-            placeholder="Password"
-            name="password"
-            value={password}
-            onChange={this.handleChange}
+          <FormMessages
+            successStatus={successStatus}
+            successMsg={`Welcome back, ${activeUser}!`}
+            errorStatus={errorStatus}
+            errorMsg={errorMsg}
           />
-        </Form.Group>
-        <Form.Button
-          color="purple"
+        </Form>
+        <Divider section horizontal content="OR" />
+        <Button
           size="big"
-          type="submit"
-          content={isProcessing ? "Processing" : "Submit"}
-          disabled={!email || !password}
+          color="facebook"
+          icon="facebook"
+          disabled={!!activeUser}
+          className="social-auth"
+          content="Log in with Facebook"
+          onClick={this.handleFacebook}
         />
-        <FormMessages
-          successStatus={successStatus}
-          successMsg={`Welcome back, ${activeUser}!`}
-          errorStatus={errorStatus}
-          errorMsg={errorMsg}
+        <Button
+          size="big"
+          color="google plus"
+          icon="google"
+          className="social-auth"
+          content="Sign up with Google"
         />
-      </Form>
+      </Fragment>
     );
   }
 }
