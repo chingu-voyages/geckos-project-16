9846e9ef92d20756771ae26c6ed0cd6c8a249de4
Daniel Strong <danielstrong.ds@gmail.com>
2019-01-25 12:55:44 +0900

    react-snapshot added to test production build

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import "@babel/polyfill";
 import "react-app-polyfill/ie11";
 import React from "react";
-import ReactDOM from "react-dom";
+import { render } from "react-snapshot";
 import "./index.css";
 import NavBar from "./components/containers/NavBar";
 import { BrowserRouter as Router } from "react-router-dom";
@@ -14,7 +14,7 @@ const Site = () => (
   </Router>
 );
 
-ReactDOM.render(<Site />, document.getElementById("root"));
+render(<Site />, document.getElementById("root"));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
