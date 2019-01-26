a7b5c54a86492d6589508a887736e1b58f85afb1
Daniel Strong <danielstrong.ds@gmail.com>
2019-01-26 11:02:03 +0900

    both logins work correctly and have been refactored

--- src/components/dynamic/AuthHolder.js
+++ src/components/dynamic/AuthHolder.js
@@ -1,8 +1,5 @@
 import React from "react";
 import { withRouter, Link } from "react-router-dom";
-import { Divider, Button } from "semantic-ui-react";
-import FacebookLogin from "react-facebook-login/dist/facebook-login-render-props";
-import GoogleLogin from "react-google-login";
 import StyledContainer from "../reusable/StyledContainer";
 import LogInForm from "./LogInForm";
 import SignUpForm from "./SignUpForm";
@@ -10,8 +7,6 @@ import "./AuthHolder.css";
 
 const AuthHolder = ({ match, handleUser }) => {
   const isLogin = match.url.slice(1) === "login";
-  const responseFacebook = response => console.log(response);
-  const responseGoogle = response => console.log(response);
   return (
     <StyledContainer
       topHeader={isLogin ? "Log into your Account" : "Create an Account"}
@@ -28,37 +23,6 @@ const AuthHolder = ({ match, handleUser }) => {
       ) : (
         <SignUpForm handleUser={handleUser} />
       )}
-      <Divider section horizontal content="OR" />
-      <FacebookLogin
-        appId="323501384952013"
-        fields="name,email,picture"
-        callback={responseFacebook}
-        render={renderProps => (
-          <Button
-            size="big"
-            color="facebook"
-            icon="facebook"
-            className="social-auth"
-            content="Login with Facebook"
-            onClick={renderProps.onClick}
-          />
-        )}
-      />
-      <GoogleLogin
-        clientId="849865718976-222rlc6r2qnuqd6m7cv9kbnra318e7h7.apps.googleusercontent.com"
-        onSuccess={responseGoogle}
-        onFailure={responseGoogle}
-        render={renderProps => (
-          <Button
-            size="big"
-            color="google plus"
-            icon="google"
-            className="social-auth"
-            content="Login with Google"
-            onClick={renderProps.onClick}
-          />
-        )}
-      />
     </StyledContainer>
   );
 };
